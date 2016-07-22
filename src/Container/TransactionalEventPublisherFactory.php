a8c214e4a658e91e816dc5a21c799592aeb9cda0
Sascha-Oliver Prolic <saschaprolic@googlemail.com>
2016-07-22 17:21:50 +0800

    rename variables

--- src/Container/TransactionalEventPublisherFactory.php
+++ src/Container/TransactionalEventPublisherFactory.php
@@ -50,19 +50,19 @@ final class TransactionalEventPublisherFactory implements
      * ];
      * </code>
      *
-     * @param string $producerName
+     * @param string $eventPublisherName
      * @param array $arguments
      * @return TransactionalEventPublisher
      * @throws Exception\InvalidArgumentException
      */
-    public static function __callStatic(string $producerName, array $arguments) : TransactionalEventPublisher
+    public static function __callStatic(string $eventPublisherName, array $arguments) : TransactionalEventPublisher
     {
         if (!isset($arguments[0]) || !$arguments[0] instanceof ContainerInterface) {
             throw new Exception\InvalidArgumentException(
                 sprintf('The first argument must be of type %s', ContainerInterface::class)
             );
         }
-        return (new static($producerName))->__invoke($arguments[0]);
+        return (new static($eventPublisherName))->__invoke($arguments[0]);
     }
 
     /**
